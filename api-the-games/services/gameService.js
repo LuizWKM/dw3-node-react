1c0e6708fda56ce55c32e598df06f6ddeab52f04
LuizWKM <luizricardowm@outlook.com>
2025-03-11 20:13:31 -0300

    :hammer:feat: Feito as rotas de cadastro de jogos e exclusão de jogos

--- api-the-games/services/gameService.js
+++ api-the-games/services/gameService.js
@@ -8,6 +8,7 @@ class gameService {
   // }
 
   // async assincrona deixe o código continuar enquanto espera a promessa  / await (usar quando necessário esperar para continuar o código) espere trazer a promessa para continuar
+  // Função para listar os jogos
   async getAll() {
     try {
       const games = await Game.find();
@@ -16,6 +17,32 @@ class gameService {
       console.log(error);
     }
   }
+
+  // Função para cadastrar jogos
+  async Create(title, platform, year, price) {
+    try {
+      const newGame = new Game({
+        // title : title
+        title,
+        platform,
+        year,
+        price,
+      });
+      // Método do mongoose para cadastrar .save()
+      await newGame.save();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // Função para deletar jogos
+  async Delete(id) {
+    try {
+      await Game.findByIdAndDelete(id);
+      console.log(`Game com a id: ${id} foi excluido.`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
-export default new gameService;
+export default new gameService();
