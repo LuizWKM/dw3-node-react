eec0752d5b924b821e8c7f55b8f1f78b566bd47f
LuizWKM <luizricardowm@outlook.com>
2025-04-22 20:14:38 -0300

    :hammer:feat: Feito front e back

---
+++ the-games-front-end-react-e-api/front-end-react/src/components/HomeContent/index.js
@@ -0,0 +1,66 @@
+import styles from "@/components/HomeContent/HomeContent.module.css";
+import Loading from "../Loading";
+import axios from "axios"
+import { useState, useEffect } from "react";
+
+const HomeContent = () => {
+  // Criando um estado para GAMES
+  const [games, setGames] = useState([]);
+
+  const [loading, setLoading] = useState([]);
+
+  // Efeito colateral
+useEffect(() => {
+  const fetchGames = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/games");
+      // console.log(response);
+      // Pegando a lista e games e colando no estado
+      setGames(response.data.games)
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  };
+  // Invocando a função
+  fetchGames(); 
+}, []); // Dependência do useEffect
+  return (
+    <>
+      <div className={styles.homeContent}>
+        {/* CARD LISTA DE JOGOS */}
+        <div className={styles.listGamesCard}>
+          {/* TITLE */}
+          <div className={styles.title}>
+            <h2>Lista de jogos</h2>
+          </div>
+          <Loading loading={loading} />
+          <div className={styles.games} id={styles.games}>
+            {/* Lista de jogos irá aqui */}
+            {games.map((game) => (
+              <ul key={game._id} className={styles.listGames}>
+                <div className={styles.gameImg}>
+                  <img src="images/game_cd_cover.png" alt="Jogo em estoque" />
+                </div>
+                <div className={styles.gameInfo}>
+                  <h3>{game.title}</h3>
+                  {/* <li>Plataforma: {game.descriptions.platform}</li>
+                  <li>Gênero: {game.descriptions.genre}</li> */}
+                  <li>Ano: {game.year}</li>
+                  <li>Preço: {game.price.toLocaleString("pt-br",{
+                    style: "currency",
+                    currency: "BRL"
+                  })}</li>
+                </div>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeContent;
